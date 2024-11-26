00daea0b9cff5db33d712020b547aa5cf7d52b30
ballursajeed <ballursajeed@gmail.com>
2024-11-26 12:24:46 +0530

    installed cors and morgan package

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,9 +1,17 @@
 import express,{Express,Request,Response} from "express"
+import cors from "cors";
+import morgan from "morgan";
 
-const app:Express = express();
+const app = express();
+app.use(express.json())
+app.use(cors())
+app.use(morgan("dev"))
 
 app.get("/",(req:Request,res:Response) => {
     res.send("Hello Friend")
 })
+
+import userRouter from "./routes/user.routes"
+app.use("/api/v1/user",userRouter)
 
 export default app
