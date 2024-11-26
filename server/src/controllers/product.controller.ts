020fe4f2075ff2e5ab17e8ffd5220dae92dfe4c3
ballursajeed <ballursajeed@gmail.com>
2024-11-26 23:50:02 +0530

    added owner field to product document

--- server/src/controllers/product.controller.ts
+++ server/src/controllers/product.controller.ts
@@ -4,7 +4,7 @@ import { uploadOnCloudinary } from "../utils/cloudinary";
 import { Product } from "../models/product.model";
 
 export const addProduct = async(req:newProductRequest, res: Response, next:NextFunction) => {
-    const {name, price, category, description, stocks} = req.body
+    const { name, price, category, description, stocks } = req.body
 
     if (!name || !price || !category || !description || !stocks) {
         return res.status(400).json({
@@ -15,23 +15,25 @@ export const addProduct = async(req:newProductRequest, res: Response, next:NextF
   
     let image;
 
-    if (req.files && Array.isArray(req.files.image) && req.files.image.length > 0) {
+    if (req.files && Array.isArray(req.files.image) && req.files.image.length > 0) { // image uploading
         const fileBuffer = req.files.image[0].buffer;
         image = await uploadOnCloudinary(fileBuffer) as AvatarType;  
     }
 
-    const post = await Product.create({
+    const product = await Product.create({
         name,
         price,
         category,
         description,
         stocks,
+        owner: req.user,
         image: image?.url || undefined
     });
 
     return res.status(201).json({
         success: true,
         message: "Product Created Successfully",
-        post
+        product
       });
-    }
+}
+
