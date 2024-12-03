3cc2f3749c2b11535eb17339590abf352ffe90d1
Ballursajeed <ballursajeed@gmail.com>
2024-12-03 23:29:01 +0530

    getLatest Product API called

--- client/src/components/GetProducts/GetProducts.tsx
+++ client/src/components/GetProducts/GetProducts.tsx
@@ -16,7 +16,7 @@ const GetProducts = ({
 
   useEffect(() => {
         const getProducts = async() => {
-              const res = await axios.get(`${SERVER}/product/getAllProducts`);
+              const res = await axios.get(`${SERVER}/product/getLatestProducts`);
               
               if (res.data.success) {
               console.log(res.data.products);
