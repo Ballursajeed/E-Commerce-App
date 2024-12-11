008ea5f446c2af70b36f46957471cdfc4a000ab6
Ballursajeed <ballursajeed@gmail.com>
2024-12-11 12:37:15 +0530

    Updated Search Component

--- client/src/components/SeachResult/SearchResult.tsx
+++ client/src/components/SeachResult/SearchResult.tsx
@@ -5,6 +5,7 @@ import { SERVER } from "../../constant";
 import Navbar from "../Navbar/Navbar";
 import SingleProduct from "../SingleProduct/SingleProduct";
 import "../ListProducts/ListProducts.css"
+import GetProducts from "../GetProducts/GetProducts";
 
 const SearchResult = () => {
 
@@ -33,54 +34,110 @@ const SearchResult = () => {
           if (query) fetchResults();
     },[query])
 
+    const [categories,setCategory] = useState([]);
+    const [isAll,setIsAll] = useState(true);
+    const [isMenuOpen, setIsMenuOpen] = useState(false);
+
+ useEffect(() => {
+     const fetchCategory = async() => {
+       const response = await axios.get(`${SERVER}/product/getAllCategories`);
+       if (response.data.success) {
+           setCategory(response.data.categories)
+       }
+     }    
+     fetchCategory()
+ },[]);
+
+ const toggleMenu = () => {
+   setIsMenuOpen((prev) => !prev);
+ };
+
+ const categoryHandler = async(category:string) => {
+      setIsAll(false)
+      const res = await axios.get(`${SERVER}/product/getProductByCategory/${category}`);
+      if (res.data.success) {
+        setProducts(res.data.products)
+      } 
+ }
+
+ const priceSort = async(sort:string) => {
+   setIsAll(false)
+   const res = await axios.get(`${SERVER}/product/price/${sort}`);
+   if (res.data.success) {
+     setProducts(res.data.products)
+   } 
+ }
+
+ const maxPriceSorting  = async(price:Number) => {
+   setIsAll(false)
+
+   const res = await axios.get(`${SERVER}/product/price/${price}`);
+   if (res.data.success) {
+     setProducts(res.data.products)
+   } 
+
+ }
+
+
   return (
     <div>
     <Navbar />  
     <div className="listContainer">
-    <div className="listLeft">
-    <h1>FILTER</h1>
-    <span>Sort</span>
-    <div className="priceLong">
-      <select>
-        <option value="">None</option>
-        <option value="asc">Price (Low to High)</option>
-        <option value="dsc">Price (High to Low)</option>
-      </select>
-      <div className="price-range-container">
-        <span>Max Price: {maxPrice || ""}</span>
-        <input
-          type="range"
-          min={100}
-          max={100000}
-          value={maxPrice}
-          onChange={(e) => setMaxPrice(Number(e.target.value))}
-        />
+    <button id='ham' className="listhHamburger" onClick={toggleMenu}>
+      <span className="icon">🔍</span> Filters
+      </button>
+      <div className = {`listLeft ${isMenuOpen ? "open" : ""}`}>
+      <h1>FILTER</h1>
+      <span>Sort</span>
+      <div className="priceLong">
+        <select>
+          <option value="" onClick={() => setIsAll(true)}>None</option>
+          <option value="asc" onClick={() => priceSort('asc')} >Price (Low to High)</option>
+          <option value="dsc" onClick={() => priceSort('dsc')}>Price (High to Low)</option>
+        </select>
+        <div className="price-range-container">
+          <span>Max Price: {maxPrice || ""}</span>
+          <input
+            type="range"
+            min={100}
+            max={100000}
+            value={maxPrice}
+            onChange={(e) => setMaxPrice(Number(e.target.value))}
+          />
+          <button onClick={() => maxPriceSorting(maxPrice)}>Go</button>
+        </div>
+        <span>Category</span>
+        <select>
+          <option onClick={() => setIsAll(true)}>All</option>
+          {
+            categories.map(
+              (category) => 
+              <option onClick={() => categoryHandler(category)}>
+                {category}
+              </option> )
+          }
+        </select>
       </div>
-      <span>Category</span>
-      <select>
-        <option>All</option>
-        <option>Phone</option>
-        <option>Books</option>
-      </select>
     </div>
-  </div>
       <div className="listRight">
       <h1>Products:</h1>
-  <div className='productContainer'> 
     
     {
     products.length === 0 ?
     <>
      <h1 id="empty">No Results Found!</h1>
     </> : 
-     (
-        products.map((product,index) => {
+     ( !isAll ? 
+  <div className='productContainer'> 
+   {
+     products.map((product,index) => {
     
-            return <SingleProduct key={index} product={product}/>
-          })
-     ) 
+      return <SingleProduct key={index} product={product}/>
+    })
+   }
+  </div>
+      : <GetProducts />) 
      }
-     </div>
     </div>
    </div>
     
