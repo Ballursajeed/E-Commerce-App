3fc23f3b3fa22238444f4d37070507b82c4f1066
Ballursajeed <ballursajeed@gmail.com>
2024-12-15 12:48:08 +0530

    created dashboard component

---
+++ client/src/components/Dashboard/Dashboard.tsx
@@ -0,0 +1,15 @@
+import Navbar from "../Navbar/Navbar";
+import "./Dashboard.css";
+                                      
+const Dashboard = () => {
+  return (
+   <>
+    <Navbar />
+    <div className="dashboard">
+      hello seller
+    </div>
+   </>
+  )
+}
+
+export default Dashboard
